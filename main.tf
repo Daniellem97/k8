3c23cddff584bb3e24d30d69905ceb9ff9c64d42
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-04-19 10:23:03 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,43 @@
-
 resource "kubernetes_namespace" "example" {
   metadata {
     name   = "example-namespace"
     labels = {
       "env" = "test"
+    }
+  }
+}
+
+resource "kubernetes_deployment" "test" {
+  metadata {
+    name      = "test-deployment"
+    namespace = kubernetes_namespace.example.metadata[0].name
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "demo"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "demo"
+        }
+      }
+
+      spec {
+        container {
+          image = "nonexistent-image:latest" # This image does not exist
+          name  = "demo"
+
+          # Adding a command that exits with error to ensure failure
+          command = ["sh", "-c", "exit 1"]
+        }
+      }
     }
   }
 }
